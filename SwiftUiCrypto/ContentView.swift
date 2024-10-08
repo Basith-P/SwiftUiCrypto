180126a978af8caf25631ce9554b8589fb5b79dc
Basith P <basithpofficial@gmail.com>
2024-10-08 10:07:00 +0530

    Initial Commit

---
+++ SwiftUiCrypto/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUiCrypto
+//
+//  Created by Basith on 08/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUiCrypto/SwiftUiCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiCryptoApp.swift
+//  SwiftUiCrypto
+//
+//  Created by Basith on 08/10/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
